5d3b70df360a96c7fc3adf6ff90055c8820ecc0c
Robert <robertwright225@gmail.com>
2017-10-04 12:41:24 -0400

    Final Commit for Lab3

--- app/src/main/java/com/example/robert/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/robert/androidlabs/LoginActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
@@ -18,16 +20,20 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        SharedPreferences userEmail = getSharedPreferences(email, 0);
-        SharedPreferences.Editor editor = userEmail.edit();
-        editor.putString("DefaultEmail", "email@domain.com");
-        editor.commit();
+        final SharedPreferences userEmail = getSharedPreferences(email, 0);
 
+        final TextView etxt = findViewById(R.id.textView2);
+        etxt.setText(userEmail.getString("DefaultEmail", ""));
         loginButton = (Button) findViewById(R.id.button2);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String savedEamil = etxt.getText().toString();
+                SharedPreferences.Editor editor = userEmail.edit();
+                editor.putString("DefaultEmail", savedEamil);
+                editor.commit();
+
                 Intent intent = new Intent(LoginActivity.this, StartActivity.class);
                 startActivity(intent);
 
